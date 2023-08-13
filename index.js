e93d408e8ea03e7a077eee8985963925f8035dca
MartinAnzaldop <martinanzaldo1999@gmail.com>
2023-08-13 17:15:23 -0600

    se agrego cors y conexion a db

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
+const {dbConnection} = require("./database/config");
+const cors = require("cors");
+require("dotenv").config();
 
 //sercidor de express
 const app = express();
+dbConnection();
+app.use(cors());
 
 //rutas
-app.get('/', (req, res)=>{
-res.json({
-    ok: true,
-    msg: 'Hola mundo'
-})
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,  
+    msg: "Hola mundo",
+  });
 });
 
-
-app.listen(3000, () => {
-    console.log('servidor4 corriendo en puerto 3000');
+app.listen(process.env.PORT, () => {
+  console.log("servidor4 corriendo en puerto: "+process.env.PORT);
 });
